1cb96dee4c3bbd5b51f6b9036f6ebf89de49d15b
Rizlan Mohammed <rizlanalm@outlook.com>
2023-07-17 10:44:17 +0530

    removed unwanted imports

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './.env' })
-const mongoose = require('mongoose');
+dotenv.config({ path: './.env' });
 const app = require('./app');
 const connectDB = require('./db/config');
 
